c524032a2d47555664c6840b370160e1876d3f1e
Gowtham Raj <gowthrj@amazon.com>
2022-06-06 16:48:27 +0530

    Added shapes for Login Buttons and Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,14 @@
 import React from "react";
-
+import './Navbar.css';
 
 export default function Navbar()
 {
     return(
-        <h1>Navbar working</h1>
-
-
-
+        <>
+        <div className="navbar"></div>
+        <div className="loginCard1"></div>
+        <div className="loginCard2"></div>
+        <div className="loginCard3"></div>
+        </>
     )
 }
